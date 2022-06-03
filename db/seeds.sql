a71b5b213973066630f160b9d413f0f07984c727
GDav24 <gdav24@gmail.com>
2022-06-02 21:39:04 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (username, email, password) VALUES
+		("JuanNieves", "Jn@gmail.com", "jn43"),
+        ("GustavoDavila", "gustavod@gmail.com", "Gustavo020"),
+	    ("JavierDelgado", "Javier@gmail.com", "javier20");
+
+INSERT INTO posts (title, post_url, user_id) VALUES
+    ("Taskmaster goes public!","https://taskmaster.com/press", 1),
+    ("Runbuddy reaches 1 million subscribers","https://runbuddy.com/press", 1);
